7f78d27e9a387cf39f0330b5bb4b03826c2a34ac
jacksonk1868 <jacksonk1868@student.faytechcc.edu>
2024-06-14 02:13:54 -0400

    Add files via upload

---
+++ P1LAB1 KLJ.py
@@ -0,0 +1,16 @@
+#Kayla Jackson
+#6/13/24
+#P1Lab1
+#This project allows the user to enter at command prompt first and last name
+#so the output is "Hello, First name input, Last name input".
+
+
+
+
+
+
+
+
+firstName = input("Enter Your First Name")
+lastName = input("Enter Your Last Name")
+print("Hello:" + firstName + " " + lastName)
